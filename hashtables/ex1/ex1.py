5d446cdfe03818ba731ff9b5d6e6a655557ef527
Rob Hamilton <RobHamilton3rd@gmail.com>
2020-03-13 11:51:45 -0400

    first hash table exercise

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -9,10 +9,36 @@ from hashtables import (HashTable,
 def get_indices_of_item_weights(weights, length, limit):
     ht = HashTable(16)
 
-    """
-    YOUR CODE HERE
-    """
 
+    #If there are less than 2 packages
+    #Return None since you can't get a pair of unique packages
+    #this addresses the first unit test
+
+    if len(weights) < 2:
+        return None
+
+    #across the length of the table
+    #insert the index and the weight into a hash table
+    for i in range(0, length):
+        hash_table_insert(ht, weights[i],i)
+
+
+    #with all values inserted into the hash table
+    #we can look through the remaining indexes and weights of packages
+    #to find a valid condition
+    for(index,weight) in enumerate(weights):
+        #with the limit condition defined and the known weight of the first package
+        #the companion target weight is set by taking limit-weight
+        companion_target = limit - weight
+        #with that target weight known, we can look up that weight in the hash table at constant time!
+        companion_index = hash_table_retrieve(ht,companion_target)
+
+        #if we were able to find the value
+        if companion_index is not None:
+            #The documentation in the readme states that we want the largest value in the 0th index
+            #this sorts based on which index is value, the initial package or the companion retreived
+            #from the hash table
+            return (index,companion_index) if index > companion_index else (companion_index,index)
     return None
 
 
